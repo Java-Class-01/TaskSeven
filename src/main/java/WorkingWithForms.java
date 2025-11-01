d3c630de906ecf1cfb60a193bc2f747b5f875d11
austin wamono <austinwamono98@gmail.com>
2025-11-02 01:05:41 +0300

    commit#8: used gridbag layout manager to position the JComponents on the Panel

--- src/main/java/WorkingWithForms.java
+++ src/main/java/WorkingWithForms.java
@@ -30,6 +30,56 @@ public class WorkingWithForms {
         return MainFrame;
     }
     public JPanel prepareFormPanel() {
+        formPanel = new JPanel();
+        formPanel.setLayout(new GridBagLayout());
+        formPanel.setBorder(BorderFactory.createTitledBorder("Employee Information"));
+        formPanel.setPreferredSize(new Dimension(400, 500));
+        formPanel.setBackground(new Color(240, 240, 255));
+
+        //chose gridbag as our layoutmanager
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(8, 8, 8, 8);
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+
+        int y = 0;
+
+        //the fullname to be arranged as the first item
+        gbc.gridx = 0; gbc.gridy = y;
+        formPanel.add(prepareJLabelFullName(), gbc);
+        gbc.gridx = 1;
+        formPanel.add(PrepareJTextFieldFullName(), gbc);
+        y++;
+
+        //the email to be the second on the list
+        gbc.gridx = 0; gbc.gridy = y;
+        formPanel.add(prepareJLabelEmailL(), gbc);
+        gbc.gridx = 1;
+        formPanel.add(prepareJTextFieldEmailInput(), gbc);
+        y++;
+
+        //the password appears next
+        gbc.gridx = 0; gbc.gridy = y;
+        JLabel passwordLabel = new JLabel("Password:");
+        formPanel.add(passwordLabel, gbc);
+        gbc.gridx = 1;
+        formPanel.add(prepareJPasswordFieldPasswordInput(), gbc);
+        y++;
+
+        //the position for department
+        gbc.gridx = 0; gbc.gridy = y;
+        formPanel.add(prepareJLabelDepartment(), gbc);
+        gbc.gridx = 1;
+        formPanel.add(prepareJComboBoxDepartment(), gbc);
+        y++;
+
+        // the position for DOB
+        gbc.gridx = 0; gbc.gridy = y;
+        formPanel.add(prepareJLabelDOB(), gbc);
+        gbc.gridx = 1;
+        formPanel.add(prepareJCalendarDOB(), gbc);
+        y++;
 
         return formPanel;
     }

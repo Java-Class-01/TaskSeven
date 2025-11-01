7a4ea0fe824b9351c1455df2b184709d7f73e02c
austin wamono <austinwamono98@gmail.com>
2025-11-02 00:24:47 +0300

    setting up the project files

---
+++ src/main/java/WorkingWithForms.java
@@ -0,0 +1,12 @@
+import com.toedter.calendar.JCalendar;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WorkingWithForms {
+
+    public WorkingWithForms() {
+    }
+}
